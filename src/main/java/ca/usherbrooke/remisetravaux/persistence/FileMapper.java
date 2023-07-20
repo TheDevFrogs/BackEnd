33e7899e4d3ba5b92946e6b95122365e934fb34d
Rex1o <matlavriv@gmail.com>
2023-07-19 22:25:53 -0400

    yeahh

--- src/main/java/ca/usherbrooke/remisetravaux/persistence/FileMapper.java
+++ src/main/java/ca/usherbrooke/remisetravaux/persistence/FileMapper.java
@@ -11,10 +11,13 @@ public interface FileMapper {
     @Options(useGeneratedKeys = true, keyProperty = "id_file", keyColumn = "id_file")
     void insertFile(@Param("f") DatabaseFile file);
 
-    @Select("SELECT COALESCE( " +
-            "(SELECT 1 " +
-            "FROM assignmentfileinfos " +
-            "WHERE cip = #{cip} AND id_file = #{id_file}), 0)")
+    @Select("SELECT COALESCE(" +
+            "            (SELECT 1" +
+            "             FROM file " +
+            "                INNER JOIN assignment a on file.id_file = a.id_file " +
+            "                inner join groupmember g on a.id_group = g.id_group " +
+            "             WHERE file.id_file = #{id_file} AND cip = #{cip} " +
+            "    LIMIT 1), 0)")
     boolean canDownloadAssignmentFile(@Param("cip") String cip, @Param("id_file") int id_file);
 
     @Select("SELECT f.id_file, f.name, f.displayed_name, f.extension, f.path " +
